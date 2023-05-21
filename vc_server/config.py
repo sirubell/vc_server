e9f4ac4897d54d3ba34fca2c0ca453f61118250e
sirubell <sirius65535@gmail.com>
2023-05-22 02:53:15 +0800

    remove some env variable

--- vc_server/config.py
+++ vc_server/config.py
@@ -10,10 +10,6 @@ mail_from = os.getenv("MAIL_FROM")
 mail_port = int(os.getenv("MAIL_PORT"))
 mail_server = os.getenv("MAIL_SERVER")
 secret = os.getenv("SECRET")
-algorithm = os.getenv("ALGORITHM")
-access_token_expire_minutes = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-need_email_validation = os.getenv("EMAIL_VALIDATION", "false").lower() in (
-    "true",
-    "1",
-    "t",
-)
+algorithm = "HS256"
+access_token_expire_minutes = 30
+need_email_validation = False
