2da7f0fdfb9f501d822076ac6110ef4e6c7db2a4
sirubell <sirius65535@gmail.com>
2023-05-22 16:37:26 +0800

    change configs

--- vc_server/config.py
+++ vc_server/config.py
@@ -4,12 +4,13 @@ import os
 load_dotenv(".env")
 
 
+need_email_validation = False
 mail_username = os.getenv("MAIL_USERNAME")
 mail_password = os.getenv("MAIL_PASSWORD")
 mail_from = os.getenv("MAIL_FROM")
 mail_port = int(os.getenv("MAIL_PORT"))
 mail_server = os.getenv("MAIL_SERVER")
+
 secret = os.getenv("SECRET")
 algorithm = "HS256"
 access_token_expire_minutes = 30
-need_email_validation = False
